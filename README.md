d59b1f93149791aabfbacc4579910020e5573a60
Andrew Hong <andrewhong@myyahoo.com>
2022-10-18 18:46:12 -0400

    Upload initial code

---
+++ README.md
@@ -0,0 +1,13 @@
+# Oral Examination 1 Simulator (Lesson 1 - 3)
+Created for Rutgers University - New Brunswick's Elementary Korean 2022 Fall Semester course (01:574:101:01).
+
+## Syntax
+
+For prompts,
+ - `-` means the program will give the question
+ - `*` means the program will skip the question
+ - Anything after `#` will be considered as a comment
+
+## Credits
+
+Maintained & created by Andrew Hong | Class of 2026
---
+++ src/utils.py
@@ -0,0 +1,47 @@
+from typing import List
+import itertools
+
+
+def parse_prompt(prompt: str) -> List[str]:
+    """Parses a prompt into different variants if specified."""
+    prompts = []
+
+    stack = 0
+    start_index = None
+    results = {}
+    for i, c in enumerate(prompt):
+        if c == "[":
+            if stack == 0:
+                start_index = i + 1
+            stack += 1
+        elif c == "]":
+            stack -= 1
+            if stack == 0:
+                out = prompt[start_index:i].split(", ")
+                results[prompt[start_index - 1 : i + 1]] = out
+
+    possibitilies = list(itertools.product(*results.values()))
+    for x in possibitilies:
+        current_prompt = prompt
+        for i, y in enumerate(x):
+            placeholder = list(results.keys())[i]
+            current_prompt = current_prompt.replace(placeholder, y)
+
+        prompts.append(current_prompt)
+
+    return prompts
+
+
+def get_prompts(file_path: str) -> List[str]:
+    """Reads a file and parses all prompts"""
+    file = open(file_path, mode="r", encoding="utf-8")
+    raw_prompts = file.read().split("\n")
+
+    prompts = []
+    for l in raw_prompts:
+        if l.startswith("* "):
+            continue
+        elif l.startswith("- "):
+            prompts += parse_prompt(l.replace("- ", ""))
+
+    return prompts
